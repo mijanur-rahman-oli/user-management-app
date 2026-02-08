21a3492cb870afd38d49b9d20d1ec38cfb5a857a
Mijanur Rahman Oli <sheikhmijanurrahmanoli@gmail.com>
2026-02-08 12:07:42 +0600

    final push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  root: 'src', 
   plugins: [react()],
+  
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  
   server: {
     port: 3000,
     proxy: {
